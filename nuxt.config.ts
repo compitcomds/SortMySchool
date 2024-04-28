46d4df7b61df7dd354deb0f1ae0b993fb2b5196b
stocksalesdata <theaimcreator@gmail.com>
2024-04-29 02:26:37 +0530

    asd

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  plugins: ["~/plugins/preline.client.ts"],
+  colorMode: {
+    preference: 'light', // Force light theme
+    fallback: 'light', // Fallback theme if user's preference cannot be determined
+    classSuffix: '', // No suffix added to the class
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*", './node_modules/preline/preline.js',],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui'),require('preline/plugin')],
+  daisyui: {
+    themes: ["light","dark"]
+  },
+}
+
