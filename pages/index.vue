0859a99fbc3e4058dac6a36d0f8231068dbd7b35
compitcomds <compitcomds@gmail.com>
2024-04-29 23:28:02 +0530

    efrewfth

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
             class="inline-flex items-center gap-2 bg-violet-700 text-white text-lg font-semibold py-3 px-6 rounded-r-md">
             <span>Search</span>
             <span class="hidden md:block">
-                <svg class="text-gray-200 h-5 w-5 p-0 fill-current" xmlns="http://www.w3.org/2000/svg"
+                <svg class="text-white h-5 w-5 p-0 fill-current" xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px"
                     viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
                     width="512px" height="512px">
@@ -69,6 +69,62 @@
             </div>
         </div>
     </div>
+  </section>
+  <section>
+    <div class="py-5 text-black">
+        <div class="container flex flex-col items-center justify-center w-full p-6 mx-auto text-center xl:px-0">
+            <div class="text-lg font-bold tracking-wider text-indigo-600 uppercase">Testimonials</div>
+            <h2 class="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl ">Here's what our customers said</h2>
+            <p class="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe, inventore sapiente corporis facilis alias..</p>
+        </div>
+        <div class="container p-6 mx-auto mb-10 xl:px-0">
+            <div class="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
+                <div class="lg:col-span-2 xl:col-auto">
+                    <div class="flex flex-col justify-between w-full h-full px-6 py-6 bg-gray-100  md:px-14 rounded-2xl md:py-14 ">
+                        <p class="text-2xl leading-normal ">Share a <mark class="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4">testimonial</mark> that hits some of your benefits from one of your popular customers.</p>
+                        <div class="flex items-center mt-8 space-x-3">
+                            <div class="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+                                <img alt="Avatar" src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?crop=faces&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;ixid=MnwxfDB8MXxhbGx8fHx8fHx8fHwxNjIwMTQ5ODEx&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=100&amp;h=100" loading="lazy" />
+                            </div>
+                            <div>
+                                <div class="text-lg font-medium text-black">Sarah Steiner</div>
+                                <div class="text-gray-600 ">VP Sales at Google</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="flex flex-col justify-between w-full h-full px-6 py-6 bg-gray-100  md:px-14 rounded-2xl md:py-14 dark:bg-trueGray-800">
+                        <p class="text-2xl leading-normal text-black">Make sure you only pick the <mark class="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 ">right sentence</mark> to keep it short and simple.</p>
+                        <div class="flex items-center mt-8 space-x-3">
+                            <div class="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+                                <img alt="Avatar" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;crop=faces&amp;fit=crop&amp;w=100&amp;h=100&amp;q=80" loading="lazy" />
+                            </div>
+                            <div>
+                                <div class="text-lg font-medium text-black">Dylan Ambrose</div>
+                                <div class="text-gray-600 ">Lead marketer at Netflix </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="flex flex-col justify-between w-full h-full px-6 py-6 bg-gray-100  md:px-14 rounded-2xl md:py-14 dark:bg-trueGray-800">
+                        <p class="text-2xl leading-normal text-black">This is an <mark class="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 ">awesome</mark> landing page template I've seen. I would use this for anything.</p>
+                        <div class="flex items-center mt-8 space-x-3">
+                            <div class="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+                                <img alt="Avatar" src="https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=100&amp;h=100&amp;crop=faces&amp;q=80" loading="lazy" />
+                            </div>
+                            <div>
+                                <div class="text-lg font-medium text-black">Gabrielle Winn</div>
+                                <div class="text-gray-600 ">Co-founder of Acme Inc</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
   </section>
 </template>
 
