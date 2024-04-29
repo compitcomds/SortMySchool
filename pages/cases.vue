1edcd674dec1449dfd076c3416a2f661fc2cd480
Vikas-Maur <68019002+Vikas-Maur@users.noreply.github.com>
2024-04-29 23:30:04 +0530

    Added all subjects on cases page

--- pages/cases.vue
+++ pages/cases.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>
-        <h1>cases</h1>
-    </div>
+  <div>
+    <h1>cases</h1>
+    <ul>
+      <li v-for="subject in subjects" :key="subject.id">
+        {{ subject.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+import { ref, onMounted } from "vue";
+import { getAllSubjects } from "~/utils/appwrite";
+const subjects = ref([]);
+
+async function fillAllSubjects() {
+  const subs = await getAllSubjects();
+  subjects.value = subs.documents;
+  console.log(subs);
+}
+
+onMounted(fillAllSubjects);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
