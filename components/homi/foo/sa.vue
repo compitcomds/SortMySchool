82dd6e55b0c45e64d18bd2e66616a3a9ba894752
compitcomds <compitcomds@gmail.com>
2024-04-29 14:20:16 +0530

    df

---
+++ components/homi/foo/sa.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-3 m-2 bg-red-800 text-white">
+this is sample alter
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/asd.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+this is asd page
+<sample/>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
