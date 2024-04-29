96b1c098a8b0956914c65927d476f7ca107a5d52
compitcomds <compitcomds@gmail.com>
2024-04-29 17:29:00 +0530

    asdfghjkl

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-
+    <section class="bg-cover bg-center bg-w3schools h-[300px] flex items-center">
+        <div class="container mx-auto px-4">
+            <div class="bg-white bg-opacity-50 rounded-lg p-4 mx-auto max-w-md">
+                <!-- Search bar goes here -->
+                <div class="bg-white bg-opacity-50 rounded-lg p-4 mx-auto max-w-md">
+                    <input type="text" placeholder="Search..." class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500">
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    
 
 </template>
 
@@ -9,4 +20,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style  scoped>
+.bg-w3schools {
+    background-image: url('C:\Users\1999g\OneDrive\Desktop\SortMyLawSchool\SortMySchool\assets\background_in_space.gif');
+}
+</style>
