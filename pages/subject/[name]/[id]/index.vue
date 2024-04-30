da69f8903480cd09d22323fdac6a63e9f6ef0dfd
Vikas-Maur <68019002+Vikas-Maur@users.noreply.github.com>
2024-04-30 19:59:04 +0530

    Added fetch blog content in the blogs page

--- pages/subject/[name]/[id]/index.vue
+++ pages/subject/[name]/[id]/index.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+    import { getBlogById } from "~/utils/appwrite";
+
     export default {
         data(){
             return{
@@ -27,12 +29,21 @@
             return this.$route.params.id || ''; 
         }
     },
+    mounted(){
+        this.getDataById();
+    },
     methods: {
         // here write a login to get data
         // for get data use this keyword 
         
-        getDataById(){
-            this.content='as'
+        async getDataById(){
+            try {
+                const blog = await getBlogById(this.id);
+                this.content = blog.content;
+                console.log(blog);
+            } catch (error) {
+                console.error("Error fetching blog:", error);
+            }
 
         }
     },
