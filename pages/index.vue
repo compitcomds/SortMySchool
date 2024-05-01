9a7b0fd8d036a6a6181755e21a023d2df223fc51
compitcomds <compitcomds@gmail.com>
2024-05-01 23:30:21 +0530

    etregrg

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <section class="bg-cover bg-center bg-w3schools lg:min-h-[600px] min-h-[300px] pb-14 flex flex-col items-center justify-center relative">
+    <section
+        class="bg-cover bg-center bg-w3schools lg:min-h-[600px] min-h-[300px] pb-14 flex flex-col items-center justify-center relative">
         <img src="assets\lynx_in_space.png" alt="Background Image"
             class="hidden sm:block absolute right-0 lg:pr-[120px] md:block h-auto w-auto md:max-w-none" />
         <div class="mb-8 text-white flex flex-col items-center justify-center">
@@ -97,7 +98,69 @@
             </div>
         </div>
     </section>
+    <!-- how it works -->
 
+    <section>
+        <!-- Container -->
+        <div class="mx-auto max-w-7xl px-5 py-8 md:px-10 md:py-8 lg:py-8">
+            <!-- Heading Div -->
+            <div class="mx-auto w-full max-w-3xl">
+                <!-- Component -->
+                <div class="text-center">
+                    <p class="uppercase text-[#1353fe]">3 easy steps</p>
+                    <h2 class="text-3xl font-semibold capitalize md:text-5xl">How it <span
+                            class="bg-[url('https://assets.website-files.com/63904f663019b0d8edf8d57c/639156ce1c70c97aeb755c8a_Rectangle%2010%20(1).svg')] bg-cover bg-center bg-no-repeat px-4 text-white">works</span>
+                    </h2>
+                    <div class="mx-auto mb-8 mt-4 max-w-[528px] md:mb-12 lg:mb-16">
+                        <p class="text-[#636262]">Lorem ipsum dolor sit amet consectetur adipiscing elit ut
+                            aliquam,purus sit amet luctus magna fringilla urna</p>
+                    </div>
+                </div>
+            </div>
+            <!-- How it Works Div -->
+            <div class="mx-auto grid grid-cols-1 gap-4 sm:justify-items-stretch md:grid-cols-3 lg:gap-8">
+                <!-- How it Works Item -->
+                <div class="relative flex flex-col items-center gap-4 p-8 text-center">
+                    <div
+                        class="mb-5 flex max-w-[400px] flex-col items-center justify-center rounded-xl border border-solid border-black bg-white px-8 py-5 [box-shadow:rgb(0,_0,_0)_4px_4px] md:mb-6 lg:mb-8">
+                        <p class="text-xl font-bold">1</p>
+                    </div>
+                    <p class="mb-2 text-xl font-semibold">Find Component</p>
+                    <p class="text-sm text-[#636262]">Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam,
+                        purus sit.</p>
+                    <img src="https://assets.website-files.com/63904f663019b0d8edf8d57c/639833af1925275b6f0b43e1_Vector%2032.svg"
+                        alt=""
+                        class="absolute bottom-[-33%] left-0 right-auto top-auto -z-10 hidden w-96 md:bottom-auto md:left-[136px] md:right-[-50%] md:top-[18%] md:inline-block lg:left-auto" />
+                </div>
+                <!-- How it Works Item -->
+                
+                <div class="relative flex flex-col items-center gap-4 p-8 text-center">
+                    <div
+                        class="mb-5 flex max-w-[400px] flex-col items-center justify-center rounded-xl border border-solid border-black bg-white px-8 py-5 [box-shadow:rgb(0,_0,_0)_4px_4px] md:mb-6 lg:mb-8">
+                        <p class="text-xl font-bold">2</p>
+                    </div>
+                    <p class="mb-2 text-xl font-semibold">Copy and Paste</p>
+                    <p class="text-sm text-[#636262]">Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam,
+                        purus sit.</p>
+                    <img src="https://assets.website-files.com/63904f663019b0d8edf8d57c/639834731925279c5e0b4ee5_Vector%2033.svg"
+                        alt=""
+                        class="absolute bottom-[-33%] left-0 right-auto top-auto -z-10 hidden w-96 md:bottom-auto md:left-[136px] md:right-[-50%] md:top-[8%] md:inline-block lg:left-auto" />
+                </div>
+                <!-- How it Works Item -->
+                <div class="relative flex flex-col items-center gap-4 p-8 text-center">
+                    <div
+                        class="mb-5 flex max-w-[400px] flex-col items-center justify-center rounded-xl border border-solid border-black bg-white px-8 py-5 [box-shadow:rgb(0,_0,_0)_4px_4px] md:mb-6 lg:mb-8">
+                        <p class="text-xl font-bold">3</p>
+                    </div>
+                    <p class="mb-2 text-xl font-semibold">Done!</p>
+                    <p class="text-sm text-[#636262]">Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam,
+                        purus sit.</p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!-- end section works -->
     <!-- Testimonial -->
 
     <section class="bg-white ">
@@ -200,7 +263,7 @@
                             </a>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
