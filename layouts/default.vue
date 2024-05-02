1b142077e7067d0ef6d8e0ade42d5e55e36e60ab
compitcomds <compitcomds@gmail.com>
2024-05-02 12:05:34 +0530

    fgfdbab

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,12 +19,33 @@
         <!-- here is cuted dipesh nav  -->
     </header>
     <slot />
-    <myfooter/>
+    <myfooter />
+    <!-- back to top button -->
+    <div class="fixed bottom-0 right-0 p-4">
+        <button class="bg-indigo-800 text-white rounded-full w-10 h-10 flex items-center justify-center" @click="backToTop">
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18">
+              </path>
+          </svg>
+        </button>
+      </div>
 </template>
 
+
 <script>
-export default {};
+    export default {
+  methods: {
+    backToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // This smoothens the scroll
+      });
+    }
+  }
+}
 </script>
+
 
 <style scoped>
 .nav-item {
