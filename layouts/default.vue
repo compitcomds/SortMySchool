bf455125c584b4c4c7867016fae6c66a8d88b35a
compitcomds <compitcomds@gmail.com>
2024-05-04 21:44:16 +0530

    sad

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,15 +49,9 @@ export default {
 
 
 <style>
-/* .nav-item {
-  @apply font-medium text-gray-500 py-3 md:py-6 dark:text-neutral-400;
+body{
+  background-color: aliceblue;
 }
-
-.nav-active,
-.router-link-exact-active {
-  @apply font-medium text-blue-600 py-3 md:py-6 dark:text-blue-500;
-} */
-
 * {
   -webkit-user-select: none;
   /* Safari */
