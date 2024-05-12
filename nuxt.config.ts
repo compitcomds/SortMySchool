de5019a4e2a9a51a5fa54906ed283b54d1cd86e8
Vikas-Maur <68019002+Vikas-Maur@users.noreply.github.com>
2024-05-12 13:21:36 +0530

    Fixed some issues in prerendering of search page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  plugins: ["~/plugins/preline.client.ts", { src: '~/plugins/disableRightClick.ts', mode: 'client' }],
+  plugins: [
+    "~/plugins/preline.client.ts",
+    { src: "~/plugins/disableRightClick.ts", mode: "client" },
+  ],
   colorMode: {
-    preference: 'light', // Force light theme
-    fallback: 'light', // Fallback theme if user's preference cannot be determined
-    classSuffix: '', // No suffix added to the class
+    preference: "light", // Force light theme
+    fallback: "light", // Fallback theme if user's preference cannot be determined
+    classSuffix: "", // No suffix added to the class
   },
   routeRules: {
-    "/subject": { swr: 60 }, // 5 minutes,
-    "/subject/**": { swr: 60 }, // 5 minutes,
-    "/subject/[name]/[id]": { swr: 60 }, // 5 minutes,
-
-  }
-})
+    "/subject": { isr: 60 * 5 }, // 5 minutes,
+    "/subject/**": { isr: 60 * 5 }, // 5 minutes,
+    "/subject/[name]/[id]": { isr: 60 * 5 }, // 5 minutes,
+    "/search/**": { prerender: false },
+  },
+});
