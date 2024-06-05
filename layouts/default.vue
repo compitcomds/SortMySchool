7bff2f2065e2bc6d3dd79e475f618c75b87b0aaf
compitcomds <compitcomds@gmail.com>
2024-06-05 14:04:33 +0530

    ef

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,26 @@
   <NuxtLoadingIndicator />
   <header
     class="flex flex-col flex-wrap sm:justify-center sm:flex-nowrap z-50 w-full bg-stone-50 text-sm sm:py-0 text-black">
-    <div class="inline-flex gap-5 justify-between items-center sm:p-0 md:p-0 lg:py-6 lg:px-4 xl:max-w-7xl xl:mx-auto">
+    <div class="flex gap-5 justify-between items-center w-full lg:px-6">
 
       <div class="h-auto w-56 hidden sm:block md:block"><nuxt-link to="/">
           <NuxtImg loading="lazy" src="/assets/MainLogo.png" alt="SortMyLawSchool | Logo" srcset="" />
         </nuxt-link>
 
       </div>
-
-      <div class="object-cover lg:w-1/2 lg:rounded-lg"><nuxt-link to="https://www.youtube.com/@fglawkit">
-          <NuxtImg loading="lazy"
-            src="https://yt3.googleusercontent.com/tis5V1pB6fvLi4yEXiivr9tCfw9gXNj95uyafLNH8MNzV058-yT048b4inWqeIkIO1wHkZcTyA=w1060-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
-            alt="SortMyLawSchool | Header Banner" srcset="" class="rounded-lg" />
-        </nuxt-link>
+      <div class="flex flex-col lg:flex-row justify-end p-2 gap-3 lg:gap-5">
+        <div class="object-cover lg:w-1/3 lg:rounded-lg"><nuxt-link to="https://www.youtube.com/@fglawkit">
+            <NuxtImg loading="lazy"
+              src="https://yt3.googleusercontent.com/tis5V1pB6fvLi4yEXiivr9tCfw9gXNj95uyafLNH8MNzV058-yT048b4inWqeIkIO1wHkZcTyA=w1060-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
+              alt="SortMyLawSchool | Header Banner" srcset="" class="rounded-lg" />
+          </nuxt-link>
+        </div>
+        <div class="object-cover lg:w-1/3 lg:rounded-lg"><nuxt-link to="https://www.youtube.com/@fglawkit">
+            <NuxtImg loading="lazy"
+              src="https://yt3.googleusercontent.com/tis5V1pB6fvLi4yEXiivr9tCfw9gXNj95uyafLNH8MNzV058-yT048b4inWqeIkIO1wHkZcTyA=w1060-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
+              alt="SortMyLawSchool | Header Banner" srcset="" class="rounded-lg" />
+          </nuxt-link>
+        </div>
       </div>
     </div>
     <hr>
