e68eced95441fdf7a857a5136317ddedfc1c6678
compitcomds <compitcomds@gmail.com>
2024-09-28 14:09:42 +0530

    dfd

--- components/CarouselComponent.vue
+++ components/CarouselComponent.vue
@@ -3,21 +3,59 @@
     <!-- Left Side: Text and Button -->
     <div class="md:w-1/2 w-full mb-6 md:mb-0 md:text-left mt-6 md:mt-0 ">
       <h2 class="text-4xl font-bold mb-8">How We Help You</h2>
-      <ul class="list-disc pl-5 space-y-6">
-        <li class="text-xl">
-          <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EZD7b1d-idr6XR2Br8kxKmg&si=0KkP4LhA-qrF6Hdx" class="flex items-center hover:text-blue-500 ">Improve your English – Explore our Improve Reading Skills playlist. <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="ml-2 w-4 h-4 lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg></a>
-        </li>
-        <li class="text-xl">
-          <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EYNGuEvsmM2Ep0_gujUyDsB&si=3eg42Afmxzp9tQ13" class="flex items-center hover:text-blue-500">Get internships or jobs – Listen to our Legal Podcasts series. <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="ml-2 w-4 h-4 lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg></a>
-        </li>
-        <li class="text-xl">
-          <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EZRFmOv-NEHRq8hvNAZlCcM&si=d_wdnucjW3ncpOah" class="flex items-center hover:text-blue-500">Learn drafting – Check out our Get Practical Knowledge series. <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="ml-2 w-4 h-4 lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg></a>        </li>
-        <li class="text-xl">
-          <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EYuQXWtMDz1QXe19hhNM3eq&si=SCGodrj_O1y33uxm" class="flex items-center hover:text-blue-500">Build your CV or SoP – Visit our Law Internship playlist. <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="ml-2 w-4 h-4 lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg></a>        </li>
-        <li class="text-xl">
-          <a href="https://chat.whatsapp.com/BXUajT2udYCGk7HRXgECuq" class="flex items-center hover:text-blue-500">Legal Jobs Update – Join our WhatsApp Community. <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="ml-2 w-4 h-4 lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg></a>
-        </li>
-      </ul>
+      <ul class=" space-y-6">
+    <li class="text-xl flex items-start">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scale mr-2">
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="M7 21h10"/>
+            <path d="M12 3v18"/>
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"/>
+        </svg>
+        <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EZD7b1d-idr6XR2Br8kxKmg&si=0KkP4LhA-qrF6Hdx" class="hover:text-blue-500">Improve your English – Explore our Improve Reading Skills playlist.</a>
+    </li>
+    <li class="text-xl flex items-start">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scale mr-2">
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="M7 21h10"/>
+            <path d="M12 3v18"/>
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"/>
+        </svg>
+        <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EYNGuEvsmM2Ep0_gujUyDsB&si=3eg42Afmxzp9tQ13" class="hover:text-blue-500">Get internships or jobs – Listen to our Legal Podcasts series.</a>
+    </li>
+    <li class="text-xl flex items-start">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scale mr-2">
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="M7 21h10"/>
+            <path d="M12 3v18"/>
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"/>
+        </svg>
+        <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EZRFmOv-NEHRq8hvNAZlCcM&si=d_wdnucjW3ncpOah" class="hover:text-blue-500">Learn drafting – Check out our Get Practical Knowledge series.</a>
+    </li>
+    <li class="text-xl flex items-start">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scale mr-2">
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="M7 21h10"/>
+            <path d="M12 3v18"/>
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"/>
+        </svg>
+        <a href="https://youtube.com/playlist?list=PL-1DBVyVi7EYuQXWtMDz1QXe19hhNM3eq&si=SCGodrj_O1y33uxm" class="hover:text-blue-500">Build your CV or SoP – Visit our Law Internship playlist.</a>
+    </li>
+    <li class="text-xl flex items-start">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scale mr-2">
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z"/>
+            <path d="M7 21h10"/>
+            <path d="M12 3v18"/>
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"/>
+        </svg>
+        <a href="https://chat.whatsapp.com/BXUajT2udYCGk7HRXgECuq" class="hover:text-blue-500">Legal Jobs Update – Join our WhatsApp Community.</a>
+    </li>
+</ul>
+
 
       <!-- <a href="/subject"
         class="relative inline-flex lg:w-auto w-full justify-center lg:justify-start items-center px-12 lg:py-2 py-2 overflow-hidden text-lg font-medium text-sky-500 border-2 border-sky-500 rounded-full hover:text-white group hover:bg-gray-50">
@@ -36,14 +74,14 @@
     <!-- Right Side: Image Carousel -->
     <div class="carousel w-full md:w-1/2 h-56 md:h-[380px]">
       <div id="slide1" class="carousel-item relative w-full">
-        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/1.png" class="w-full" />
+        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/2.png" class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between ">
-          <a href="#slide4" class="btn btn-circle scale-50 md:scale-90">❮</a>
+          <a href="#slide7" class="btn btn-circle scale-50 md:scale-90">❮</a>
           <a href="#slide2" class="btn btn-circle scale-50 md:scale-90">❯</a>
         </div>
       </div>
       <div id="slide2" class="carousel-item relative w-full">
-        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/2.png" class="w-full" />
+        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/1.png" class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide1" class="btn btn-circle scale-50 md:scale-90">❮</a>
           <a href="#slide3" class="btn btn-circle scale-50 md:scale-90">❯</a>
@@ -57,9 +95,30 @@
         </div>
       </div>
       <div id="slide4" class="carousel-item relative w-full">
-        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/1.png" class="w-full" />
+        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/cv.png" class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide3" class="btn btn-circle scale-50 md:scale-90">❮</a>
+          <a href="#slide5" class="btn btn-circle scale-50 md:scale-90">❯</a>
+        </div>
+      </div>
+      <div id="slide5" class="carousel-item relative w-full">
+        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/Like%20%26%20Subscribe.png" class="w-full" />
+        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide4" class="btn btn-circle scale-50 md:scale-90">❮</a>
+          <a href="#slide6" class="btn btn-circle scale-50 md:scale-90">❯</a>
+        </div>
+      </div>
+      <div id="slide6" class="carousel-item relative w-full">
+        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/WhatsApp%20Image%202024-09-28%20at%201.44.30%20PM.jpeg" class="w-full" />
+        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide5" class="btn btn-circle scale-50 md:scale-90">❮</a>
+          <a href="#slide7" class="btn btn-circle scale-50 md:scale-90">❯</a>
+        </div>
+      </div>
+      <div id="slide7" class="carousel-item relative w-full">
+        <img src="https://ccdstest.b-cdn.net/sort%20my%20law/careousel/Qr%20Code.png" class="w-full" />
+        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide6" class="btn btn-circle scale-50 md:scale-90">❮</a>
           <a href="#slide1" class="btn btn-circle scale-50 md:scale-90">❯</a>
         </div>
       </div>
