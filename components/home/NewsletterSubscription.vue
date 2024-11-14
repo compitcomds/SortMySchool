b724aa7e01ac2ae351f9322c2861a7441d70b2fd
Dpsharma1223 <dipeshvasistha7742@gmail.com>
2024-11-14 17:11:33 +0530

    form design updates

--- components/home/NewsletterSubscription.vue
+++ components/home/NewsletterSubscription.vue
@@ -19,10 +19,18 @@
             </p>
 
             <!-- Email Input and Subscribe Button -->
+             <div class=" hidden md:block">
             <div class="flex items-center bg-[#EEF8F3] rounded-lg p-2 mt-4">
                 <input type="email" placeholder="Write your email"
                     class="flex-grow bg-transparent placeholder-[#6D9A7D] border-l border-t border-b rounded-l-xl text-gray-700 p-[10px] focus:outline-none" />
                 <button class="bg-[#0ba083] text-white px-6 py-3 rounded-md hover:bg-[#1b584c] transition-colors">
+                    Subscribe
+                </button>
+            </div></div>
+            <div class="flex flex-col items-center bg-[#EEF8F3] rounded-lg p-3 gap-2 mt-4 md:hidden">
+                <input type="email" placeholder="Write your email"
+                    class="w-full bg-transparent placeholder-[#6D9A7D] border rounded-xl text-gray-700 p-[10px] focus:outline-none" />
+                <button class="bg-[#0ba083] text-white  w-full px-6 py-3 rounded-md hover:bg-[#1b584c] transition-colors">
                     Subscribe
                 </button>
             </div>
